78d0d47d7fc6854f3c1636863183a11a3a034e90
ShlomoBluth <lomibluth@gmail.com>
2023-10-03 16:37:33 +0300

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -16,13 +16,13 @@ module.exports = defineConfig({
     REQUESTS_TESTS: true,
     RECORD_KEY: 'bb3977f6-0a80-4de1-98d3-89009fb84af6',
   },
-  // e2e: {
-  //   // We've imported your old cypress plugins here.
-  //   // You may want to clean this up later by importing these.
-  //   setupNodeEvents(on, config) {
-  //     return require('./cypress/plugins/index.js')(on, config)
-  //   },
-  //   baseUrl: 'https://dev--nakdanlive.netlify.app/',
-  //   specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-  // },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'https://dev--nakdanlive.netlify.app/',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
 })
