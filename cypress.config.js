2f28a7bce598f0fd22d47cefe40f31459bdc4d23
ShlomoBluth <lomibluth@gmail.com>
2023-10-03 16:27:23 +0300

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  video: true,
+  // video: true,
   // projectId: 'if6x7a',
   // screenshotOnRunFailure: false,
   defaultCommandTimeout: 600000,
@@ -9,20 +9,20 @@ module.exports = defineConfig({
   // reporterOptions: {
   //   configFile: 'dicta-shared/reporter-config.json',
   // },
-  env: {
-    DEV_URL: 'https://dev--nakdanlive.netlify.app/',
-    LIVE_URL: 'https://nakdanlive.dicta.org.il/',
-    TOOL_TESTS: true,
-    REQUESTS_TESTS: true,
-    // RECORD_KEY: 'bb3977f6-0a80-4de1-98d3-89009fb84af6',
-  },
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    // setupNodeEvents(on, config) {
-    //   return require('./cypress/plugins/index.js')(on, config)
-    // },
-    baseUrl: 'https://dev--nakdanlive.netlify.app/',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-  },
+  // env: {
+  //   DEV_URL: 'https://dev--nakdanlive.netlify.app/',
+  //   LIVE_URL: 'https://nakdanlive.dicta.org.il/',
+  //   TOOL_TESTS: true,
+  //   REQUESTS_TESTS: true,
+  //   // RECORD_KEY: 'bb3977f6-0a80-4de1-98d3-89009fb84af6',
+  // },
+  // e2e: {
+  //   // We've imported your old cypress plugins here.
+  //   // You may want to clean this up later by importing these.
+  //   // setupNodeEvents(on, config) {
+  //   //   return require('./cypress/plugins/index.js')(on, config)
+  //   // },
+  //   baseUrl: 'https://dev--nakdanlive.netlify.app/',
+  //   specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  // },
 })
