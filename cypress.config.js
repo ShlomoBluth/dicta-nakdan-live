3ac4882cedb752e30c930fa24c479949e0ebe722
ShlomoBluth <lomibluth@gmail.com>
2023-10-03 13:00:16 +0300

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -19,9 +19,9 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
+    // setupNodeEvents(on, config) {
+    //   return require('./cypress/plugins/index.js')(on, config)
+    // },
     baseUrl: 'https://dev--nakdanlive.netlify.app/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
